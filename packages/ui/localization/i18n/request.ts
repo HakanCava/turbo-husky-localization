ef5adf592333ec6f538fd038f259ba604c42bfae
HakanCava <hakancavaa@gmail.com>
2024-09-23 23:18:43 +0300

    feat: localization components added to project

---
+++ packages/ui/localization/i18n/request.ts
@@ -0,0 +1,14 @@
+import { getUserLocale } from '../service/locale';
+import {getRequestConfig} from 'next-intl/server';
+ 
+export default getRequestConfig(async () => {
+  // Provide a static locale, fetch a user setting,
+  // read from `cookies()`, `headers()`, etc.
+  const locale = await getUserLocale();
+
+ 
+  return {
+    locale,
+    messages: (await import(`../messages/${locale}`)).default
+  };
+});
